9538f291f16b7210f064599c8f4fb0ade6e5471f
nicolewallace09 <nicole.elisaw@gmail.com>
2020-12-08 23:51:19 -0800

    formatted date

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment'; 
 
 const MovieList = (props) => {
     const FavoriteComponent = props.favoriteComponent;
@@ -8,7 +9,7 @@ const MovieList = (props) => {
                 <div className="movie-container">
 			
                     <div className='image-container d-flex justify-content-start m-3'>
-                        <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt='movie' className="align-self-center"></img>
+                        <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt='movie'></img>
                         <div 
                             // adding function from props and adding onClick property
                             onClick={() => props.handleFavoritesClick(movie)}
@@ -18,10 +19,12 @@ const MovieList = (props) => {
                     </div>
 
                     <div className="movie-info">
-                        <p>{movie.title}</p>
-                        <p>Release Date:{movie.release_date}</p>
-                        <p>Rating: {movie.vote_average}</p>
-                        <p>{movie.overview}</p>
+                        <p className="movie-title font-weight-bold">{movie.title}</p>
+                        <p><span className="movie-date font-weight-bold">Release Date:</span> <Moment format="MMMM D, YYYY">{movie.release_date}</Moment></p>
+                        <p><span className="movie-rating font-weight-bold">Rating:</span> {movie.vote_average}</p>
+                        <p><span className="movie-overview font-weight-bold">Overview:</span><br></br>
+                            {movie.overview}
+                        </p>
                     </div>
 
                 </div>
